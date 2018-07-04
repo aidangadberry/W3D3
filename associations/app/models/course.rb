5f0274b80c2abf3194b47f71cb64792fa12e9ead
App Academy Student <appacademy@Apps-Mac-mini-9.local>
2018-07-04 14:49:38 -0700

    Add instructor and instructed_courses

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -16,11 +16,21 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     class_name: :Enrollment
     
+  belongs_to :instructor,  
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
+  
   belongs_to :prerequisite,
     primary_key: :id,
     foreign_key: :prereq_id,
     class_name: :Course
 
+  has_many :postrequisites,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course
+    
   has_many :enrolled_students,
     through: :enrollments,
     source: :user  
